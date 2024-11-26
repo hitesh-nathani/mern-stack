5536627eefe299b136c1de40fe98d62a75797686
hitesh_nathani <hitesh.nathani@groovyweb.co>
2024-11-26 12:13:23 +0530

    "update server file with logger middleware"

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = 3500;
+const { logger, logEvents } = require("./middleware/logger");
 
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use(logger);
 
-app.use("/",require("./routes/root.js"));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/root.js"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
